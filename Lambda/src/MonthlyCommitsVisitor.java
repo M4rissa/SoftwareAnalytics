8933d954cc9cbca075dd923d1a8aac3b3497b55a
Justin van der Hout <justinvdh@gmail.com>
2017-10-09 14:16:45 +0200

    now walks over all commits

--- Lambda/src/MonthlyCommitsVisitor.java
+++ Lambda/src/MonthlyCommitsVisitor.java
@@ -1,22 +1,31 @@
 
 
+import java.io.File;
+import java.util.List;
+
 import org.repodriller.domain.Commit;
 import org.repodriller.domain.Modification;
 import org.repodriller.persistence.PersistenceMechanism;
 import org.repodriller.scm.CommitVisitor;
+import org.repodriller.scm.RepositoryFile;
 import org.repodriller.scm.SCMRepository;
 
 public class MonthlyCommitsVisitor implements CommitVisitor {
 
 	@Override
 	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
-		
-		for(Modification m : commit.getModifications()) {
-			writer.write(
-					commit.getHash(),
-					m.getFileName()
-			);
-			
+
+		try {
+			repo.getScm().checkout(commit.getHash());
+			List<RepositoryFile> files = repo.getScm().files();
+			for(RepositoryFile file : files) {
+				if(!file.fileNameEndsWith("java")) continue;	
+				File soFile = file.getFile();
+				System.out.println(soFile.getPath());				
+			}
+
+		} finally {
+			repo.getScm().reset();
 		}
 	}
 
