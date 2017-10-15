e78a555e68d19bf714fc7738c749e8f22b91d2fa
tamycova <tcovacevich@uc.cl>
2017-10-15 11:41:28 +0200

    mining on 20 projects done

--- Lambda/main.py
+++ Lambda/main.py
@@ -1,26 +1,53 @@
+import json
 from utils import RepoProcessing, years
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
 
-repos_names = []
-repos_x = []
-repos_y = []
-with open("repo_names.csv", "r") as f:
-    for l in f.readlines():
-        repo_name = l.strip()
-        repos_names.append(repo_name)
-        repo_csv = "./preprocess/{}_mod.csv".format(repo_name)
-        repo = RepoProcessing(repo_csv)
-        repo.process()
-        x, y = repo.get_plot_info()
-        repos_x.append(x)
-        repos_y.append(y)
+def generate_postprocess_info(repo_names):
+    with open(repo_names, "r") as f:
+        for l in f.readlines():
+            repo_name = l.strip()
+            repo_csv = "./preprocess/{}_mod.csv".format(repo_name)
+            repo_output = "./postprocess/{}.json".format(repo_name)
+            repo = RepoProcessing(repo_csv, repo_output)
+            repo.process()
+            repo.generate_plot_info()
+
+
+def get_plotting_info(repo_names):
+    repos_names = []
+    repos_x = []
+    repos_y = []
+    with open(repo_names, "r") as f:
+        for l in f.readlines():
+            repo_name = l.strip()
+            repos_names.append(repo_name)
+            repo_output = "./postprocess/{}.json".format(repo_name)
+            with open(repo_output) as jf:
+                data = json.load(jf)
+                data = list(zip(*filter(lambda x: int(x[0]) >= years()[
+                    0][1], sorted(data.items(), key=lambda x: int(x[0])))))
+            if data:
+                repo_x, repo_y = data
+                repo_x = [int(x) for x in repo_x]
+                repos_x.append(repo_x)
+                repos_y.append(repo_y)
+            else:
+                repos_x.append([])
+                repos_y.append([])
+        return repos_x, repos_y, repos_names
+
+
+repo_names = "repo_names.csv"
+# generate_postprocess_info(repo_names)
+repos_x, repos_y, repos_names = get_plotting_info(repo_names)
+
 
 colors = list(colors.cnames)
 for i in range(len(repos_names)):
     plt.plot(repos_x[i], repos_y[i], colors[i], label=repos_names[i])
-plt.plot([1394755200000], [0], colors[i + 1])  #  release date
+plt.plot([1394755200000], [0], 'g')  #  release date
 plt.axes().set_xticks([tick[1] for tick in years()])
 plt.axes().set_xticklabels([label[0] for label in years()])
 plt.xlabel('Year')
--- Lambda/utils.py
+++ Lambda/utils.py
@@ -1,7 +1,11 @@
+import csv
+import json
+
+
 def years():
-    return [("2013", 1356998400000), ("2014", 1388534400000),
-            ("2015", 1420070400000), ("2016", 1451606400000),
-            ("2017", 1483228800000)]
+    return [("2012", 1325376000000), ("2013", 1356998400000),
+            ("2014", 1388534400000), ("2015", 1420070400000),
+            ("2016", 1451606400000), ("2017", 1483228800000)]
 
 
 class Modification:
@@ -16,8 +20,9 @@ class Modification:
 
 class RepoProcessing:
 
-    def __init__(self, repo_csv):
-        self.repo_csv = open(repo_csv, "r")
+    def __init__(self, repo_csv, repo_output):
+        self.repo_csv = open(repo_csv, "r", encoding='utf-8')
+        self.output_file = open(repo_output, "w", encoding='utf-8')
         self.repo_state = dict()
         self.modifications = dict()
         self.lambdas_per_time = dict()
@@ -27,9 +32,9 @@ class RepoProcessing:
         return sum(dic.values())
 
     def process(self):
-        for l in self.repo_csv.readlines():
-            line = l.strip().split(",")
-            date, mtype, old_path, new_path, nl = line[1:]
+        reader = csv.reader(self.repo_csv)
+        for l in reader:
+            date, mtype, old_path, new_path, nl = l[1:]
             date = int(date)
             self.dates.append(date)
             nl = int(nl)
@@ -38,9 +43,10 @@ class RepoProcessing:
             self.modifications[date].append(Modification(
                 date, mtype, old_path, new_path, nl))
         self.dates.sort()
+        self.repo_csv.close()
         return None
 
-    def get_plot_info(self):
+    def generate_plot_info(self):
         for date in self.dates:
             for mod in self.modifications[date]:
                 if mod.t == 'ADD' or mod.t == 'MODIFY' or mod.t == 'COPY':
@@ -52,8 +58,6 @@ class RepoProcessing:
                     self.repo_state.pop(mod.op, "delete")
             self.lambdas_per_time[
                 date] = self.calculate_current_lambdas(self.repo_state)
-        data = list(zip(*filter(lambda x: x[0] >= years()[
-            0][1], sorted(self.lambdas_per_time.items(), key=lambda x: x[0]))))
-        if not data:
-            return [], []
-        return data
+        json.dump(self.lambdas_per_time, self.output_file)
+        self.output_file.close()
+        return None
